f59bd8e5658058e1fa6b32117b3c34f0ea82d566
Rafael Santos <rafaelnsantos1@gmail.com>
2020-11-05 02:57:16 -0300

    fix: dependency, tinymce params

--- src/TinyMCEWidget.tsx
+++ src/TinyMCEWidget.tsx
@@ -1,23 +1,24 @@
 import Widget from '@monx/react-netlifycms/dist/Widget';
 import { Editor } from '@tinymce/tinymce-react';
 
-export const TinyMCEWidget = Widget<string>(
-  ({ onChange, value }) => {
+interface TinyMCEWidgetProps {
+  plugins?: string[];
+  toolbar: string;
+  height?: number;
+  menubar: boolean;
+}
+
+export const TinyMCEWidget = Widget<string, TinyMCEWidgetProps>(
+  ({ onChange, value, params }) => {
     return (
       <Editor
         apiKey={process.env.NEXT_PUBLIC_TINY_API_KEY}
         value={value}
         init={{
-          height: 500,
-          menubar: true,
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount',
-            'codesample',
-          ],
-          toolbar:
-            'undo redo | formatselect | bold italic backcolor | codesample | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+          height: params.height,
+          menubar: params.menubar,
+          plugins: params.plugins,
+          toolbar: params.toolbar,
         }}
         onEditorChange={(content) => onChange(content)}
       />
